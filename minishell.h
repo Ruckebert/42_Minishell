57171db6103a99f2a22d5d4366e01a9b01156014
Martin Arsenijevic <martin.arsenijevic@gmail.com>
2024-10-10 10:56:07 +0200

    added headers for allowed functions

--- minishell.h
+++ minishell.h
@@ -16,9 +16,21 @@
 # include "libft/libft.h"
 # include "pipex/pipex.h"
 
+// All allowed functions are in these headers 
 #include <unistd.h>
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <curses.h>
 
 #endif
