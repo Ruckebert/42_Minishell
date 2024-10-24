e8f49bfe7d155f8f132d550ffec8e7dac11be335
Albert Aiyakup Ruckenbauer <aruckenb@c4r6p7.42vienna.com>
2024-10-24 16:25:21 +0200

    Removed tester and builtins

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aruckenb <aruckenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 12:58:57 by aruckenb          #+#    #+#             */
-/*   Updated: 2024/10/24 14:49:31 by aruckenb         ###   ########.fr       */
+/*   Updated: 2024/10/24 16:21:23 by aruckenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,6 @@
 
 //Simply a Test Function to test certain things for the executor
 //It needs alot of work though :(
-
-//Builtin Commands
-int		builtin_cmds(char *line, t_data *core)
-{
-	if (ft_strlen(line) == 0)
-		return (-1);
-	else if (ft_strncmp(line, "pwd", ft_strlen(line)) == 0)
-		pwd(core);
-	else if (ft_strncmp(line, "cd", 2) == 0)
-		cd_com(core);
-	else if (ft_strncmp(line, "exit", ft_strlen(line)) == 0)
-		exit_com(core);
-	else if (ft_strncmp(line, "export", 6) == 0)
-		export(core);
-	else if (ft_strncmp(line, "unset", 4) == 0)
-		unset(core);
-	else if (ft_strncmp(line, "env", 3) == 0)
-		env(core);
-	else if (ft_strncmp(line, "echo", 4) == 0)
-		echo_cmd(core);
-	else if (ft_strlen(line) > 1)
-		test(core);
-	return (-1);
-}
-
 int main(int argc, char *argv[], char **env)
 {
 	t_data core;
@@ -67,7 +42,7 @@ int main(int argc, char *argv[], char **env)
       		//token = tokenize(&core);
 			//parse(&core, env, token);
 			status = builtin_cmds(core.line, &core);
-			//free(core.line);
+			free(core.line);
 			if (status >= 0)
 				exit(status);
 		}
